bea72f1ca06ed8d0eb5fa840a30538f4616de838
Konstantin Kolesnik <kk@techwire.dp.ua>
2014-10-01 17:14:03 +0300

    in progress

--- MFE.Net/Udp/DiscoveryListener.cs
+++ MFE.Net/Udp/DiscoveryListener.cs
@@ -9,27 +9,27 @@ namespace MFE.Net.Udp
     public class DiscoveryListener
     {
         #region Fields
-        //private const int pollPeriodMsec = 200; // poll period in milliseconds
         private Socket socket;
-        //private int port;
-        private string name;
-        private byte[] response;
+        private int port;
+        private string key;
+        private bool isStarted = false;
+        #endregion
+
+        #region Constructor
+        public DiscoveryListener(int port, string key)
+        {
+            this.port = port;
+            this.key = key;
+        }
         #endregion
 
         #region Public methods
-        /// <summary>
-        /// Start listen at port = [port] for request string = [name].
-        /// If received, responses with [name + "OK"].
-        /// </summary>
-        public void Start(int port, string name)
+        public void Start()
         {
-            this.name = name;
-            //this.port = port;
-
-            response = Encoding.UTF8.GetBytes(name + "OK");
-
-            if (socket == null)
+            if (!isStarted)
             {
+                isStarted = true;
+
                 socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
                 //socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
                 socket.Bind(new IPEndPoint(IPAddress.Any, port));
@@ -37,42 +37,59 @@ namespace MFE.Net.Udp
                 new Thread(Listen).Start();
             }
         }
+        public void Stop()
+        {
+            isStarted = false;
+        }
         #endregion
 
         #region Private methods
         private void Listen()
         {
+            byte[] response = Encoding.UTF8.GetBytes(key + "OK");
+
             using (socket)
             {
-                while (true)
+                while (isStarted)
                 {
                     try
                     {
-                        //if (socket.Poll(pollPeriodMsec * 1000, SelectMode.SelectRead))
-                        if (socket.Poll(-1, SelectMode.SelectRead))
+                        if (socket.Poll(100, SelectMode.SelectRead))
                         {
-                            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0); // any endpoint
-                            byte[] buffer = new byte[socket.Available];
-
-                            int bytesRead = socket.ReceiveFrom(buffer, ref remoteEP);
-                            if (bytesRead > 0)
+                            if (socket.Available > 0)
                             {
-                                string request = new string(Encoding.UTF8.GetChars(buffer));
-                                if (String.Compare(request, name) == 0)
-                                    socket.SendTo(response, remoteEP);
+                                EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                                byte[] buffer = new byte[socket.Available];
+
+                                int bytesRead = socket.ReceiveFrom(buffer, ref remoteEP);
+                                if (bytesRead > 0)
+                                {
+                                    string request = new string(Encoding.UTF8.GetChars(buffer, 0, bytesRead));
+                                    if (String.Compare(request, key) == 0)
+                                    {
+                                        try
+                                        {
+                                            socket.SendTo(response, remoteEP);
+                                        }
+                                        catch (Exception e) { }
+                                    }
+                                }
                             }
                         }
                         else
                             Thread.Sleep(10);
                     }
-                    catch
+                    catch (Exception e)
                     {
                         break;
                     }
                 }
             }
 
+            socket.Close();
             socket = null;
+
+            isStarted = false;
         }
         #endregion
     }
